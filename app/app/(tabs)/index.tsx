7387676516024101b7115bb55193fd9c9c48d229
Jeremiah <40682211+fiveminus1@users.noreply.github.com>
2025-11-26 19:25:00 -0800

    home page

--- app/app/(tabs)/index.tsx
+++ app/app/(tabs)/index.tsx
@@ -1,6 +1,9 @@
 import { useEffect, useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, StyleSheet } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
+import { Flower2 } from 'lucide-react-native';
+import * as Progress from 'react-native-progress';
+
 
 interface MoistureData {
   raw: number;
@@ -11,6 +14,7 @@ const API_URL = 'https://mocki.io/v1/bbddd701-942d-4a96-a1b6-178e1f759f21';
 
 export default function HomeScreen() {
   const [moistureValue, setMoistureValue] = useState<number>(0);
+  const [moisturePercent, setMoisturePercent] = useState<number>(0);
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState('');
 
@@ -21,6 +25,7 @@ export default function HomeScreen() {
         const res = await fetch(API_URL);
         const data = await res.json() as MoistureData;
         setMoistureValue(data.raw);
+        setMoisturePercent(data.percent);
       } catch (e) {
         if (e instanceof Error){
           setError(e.message);
@@ -54,32 +59,24 @@ export default function HomeScreen() {
   
 
   return (
-    <SafeAreaView >
-      {/* <TopBar /> */}
-      <Text style={{ fontSize: 24, fontWeight: 'bold' }}>
-        Current moisture data: 
-        Value: {moistureValue} 
+    <SafeAreaView style={styles.container}>
+      <Flower2 size={150} style={{ marginBottom: 20 }}/>
+      
+      <Progress.Bar progress={moisturePercent / 100} width={200}/>
+      <Text style={{ fontSize: 24, fontWeight: 'bold', marginTop: 30 }}>
+        Current moisture data: {moistureValue} 
       </Text>
+
+      
       
     </SafeAreaView>
   );
 }
 
 const styles = StyleSheet.create({
-  titleContainer: {
-    flexDirection: 'row',
+  container: {
+    flex: 1,
     alignItems: 'center',
-    gap: 8,
-  },
-  stepContainer: {
-    gap: 8,
-    marginBottom: 8,
-  },
-  reactLogo: {
-    height: 178,
-    width: 290,
-    bottom: 0,
-    left: 0,
-    position: 'absolute',
-  },
-});
+    justifyContent: 'center',
+  }
+})
